fc55e238f06a7ebd94b605be451fa514dccfd3eb
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-01-26 15:33:47 +0800

    创建项目

---
+++ direct-transmission-after-signature-on-the-server/signature-springboot/README.md
@@ -0,0 +1,7 @@
+---
+title: signature-springboot
+tags: 
+date: 2022-01-26 15:33:30
+id: 1643182410438331000
+---
+# 摘要
---
+++ direct-transmission-after-signature-on-the-server/signature-springboot/src/main/java/com/example/signaturespringboot/SignatureSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.signaturespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignatureSpringbootApplication.class, args);
+    }
+
+}
