931e5944b50d4ff73494d50873f5de185c977a25
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-01-26 21:55:18 +0800

    Update HelloWorld.vue

--- direct-transmission-after-signature-on-the-server/signature-vue2/src/components/HelloWorld.vue
+++ direct-transmission-after-signature-on-the-server/signature-vue2/src/components/HelloWorld.vue
@@ -1,129 +1,140 @@
-<template> 
+<template>
   <div>
     <el-upload
       :action="dataObj.host"
       :data="dataObj"
       list-type="picture"
-      :multiple="false" :show-file-list="showFileList"
+      :multiple="false"
+      :show-file-list="showFileList"
       :file-list="fileList"
       :before-upload="beforeUpload"
       :on-remove="handleRemove"
       :on-success="handleUploadSuccess"
-      :on-preview="handlePreview">
+      :on-preview="handlePreview"
+    >
       <el-button size="small" type="primary">点击上传</el-button>
-      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过10MB</div>
+      <div slot="tip" class="el-upload__tip">
+        只能上传jpg/png文件，且不超过10MB
+      </div>
     </el-upload>
     <el-dialog :visible.sync="dialogVisible">
-      <img width="100%" :src="fileList[0].url" alt="">
+      <img width="100%" :src="fileList[0].url" alt="" />
     </el-dialog>
   </div>
 </template>
 <script>
-  //  import {policy} from './policy'
-  //  import { getUUID } from '@/utils'
-import axios from 'axios'
+//  import {policy} from './policy'
+//  import { getUUID } from '@/utils'
+import axios from "axios";
 
-  export default {
-    name: 'singleUpload',
-    props: {
-      value: String
+export default {
+  name: "singleUpload",
+  props: {
+    value: String,
+  },
+  computed: {
+    imageUrl() {
+      return this.value;
     },
-    computed: {
-      imageUrl() {
-        return this.value;
-      },
-      imageName() {
-        if (this.value != null && this.value !== '') {
-          return this.value.substr(this.value.lastIndexOf("/") + 1);
-        } else {
-          return null;
-        }
-      },
-      fileList() {
-        return [{
+    imageName() {
+      if (this.value != null && this.value !== "") {
+        return this.value.substr(this.value.lastIndexOf("/") + 1);
+      } else {
+        return null;
+      }
+    },
+    fileList() {
+      return [
+        {
           name: this.imageName,
-          url: this.imageUrl
-        }]
-      },
-      showFileList: {
-        get: function () {
-          return this.value !== null && this.value !== ''&& this.value!==undefined;
+          url: this.imageUrl,
         },
-        set: function () {
-        }
-      }
+      ];
     },
-    data() {
-      return {
-        dataObj: {
-          policy: '',
-          signature: '',
-          key: '',
-          ossaccessKeyId: '',
-          dir: '',
-          host: '',
-          // callback:'',
-        },
-        dialogVisible: false
-      };
+    showFileList: {
+      get: function () {
+        return (
+          this.value !== null && this.value !== "" && this.value !== undefined
+        );
+      },
+      set: function () {},
     },
-    methods: {
-      emitInput(val) {
-        this.$emit('input', val)
+  },
+  data() {
+    return {
+      dataObj: {
+        policy: "",
+        signature: "",
+        key: "",
+        ossaccessKeyId: "",
+        dir: "",
+        host: "",
+        // callback:'',
       },
-      handleRemove() {
-        this.emitInput('');
-      },
-      handlePreview() {
-        this.dialogVisible = true;
-      },
-      beforeUpload(file) {
-        let _self = this;
-        console.log(file.__filename)
+      dialogVisible: false,
+    };
+  },
+  methods: {
+    emitInput(val) {
+      this.$emit("input", val);
+    },
+    handleRemove() {
+      this.emitInput("");
+    },
+    handlePreview() {
+      this.dialogVisible = true;
+    },
+    beforeUpload(file) {
+      let _self = this;
+      console.log(file.__filename);
 
-    axios
-      .get("http://localhost:8088/oss/getPolicy")
-      .then(function (response) {
-        console.log(response.data);
-            _self.dataObj.policy = response.data.policy;
-            _self.dataObj.signature = response.data.signature;
-            _self.dataObj.ossaccessKeyId = response.data.accessid;
-            _self.dataObj.key = response.data.dir + + new Date() + '_${filename}';
-            _self.dataObj.dir = response.data.dir;
-            _self.dataObj.host = response.data.host;
-      })
-      .catch(function (error) {
-        console.log(error);
+      axios
+        .get("http://localhost:8088/oss/getPolicy")
+        .then(function (response) {
+          console.log(response.data);
+          _self.dataObj.policy = response.data.policy;
+          _self.dataObj.signature = response.data.signature;
+          _self.dataObj.ossaccessKeyId = response.data.accessid;
+          _self.dataObj.key = response.data.dir + +new Date() + "_${filename}";
+          _self.dataObj.dir = response.data.dir;
+          _self.dataObj.host = response.data.host;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      console.log(this.dataObj);
+
+      // return new Promise((resolve, reject) => {
+      //   policy().then(response => {
+      //     _self.dataObj.policy = response.data.policy;
+      //     _self.dataObj.signature = response.data.signature;
+      //     _self.dataObj.ossaccessKeyId = response.data.accessid;
+      //     _self.dataObj.key = response.data.dir + getUUID()+'_${filename}';
+      //     _self.dataObj.dir = response.data.dir;
+      //     _self.dataObj.host = response.data.host;
+      //     resolve(true)
+      //   }).catch(err => {
+      //     console.log(err)
+      //     reject(false)
+      //   })
+      // })
+    },
+
+    handleUploadSuccess(file) {
+      console.log("上传成功...");
+      this.showFileList = true;
+      this.fileList.pop();
+      this.fileList.push({
+        name: file.name,
+        url:
+          this.dataObj.host +
+          "/" +
+          this.dataObj.key.replace("${filename}", file.name),
       });
-      console.log(this.dataObj)
-  
-
-        // return new Promise((resolve, reject) => {
-        //   policy().then(response => {
-        //     _self.dataObj.policy = response.data.policy;
-        //     _self.dataObj.signature = response.data.signature;
-        //     _self.dataObj.ossaccessKeyId = response.data.accessid;
-        //     _self.dataObj.key = response.data.dir + getUUID()+'_${filename}';
-        //     _self.dataObj.dir = response.data.dir;
-        //     _self.dataObj.host = response.data.host;
-        //     resolve(true)
-        //   }).catch(err => {
-        //     console.log(err)
-        //     reject(false)
-        //   })
-        // })
-      },
-      
-      handleUploadSuccess(file) {
-        console.log("上传成功...")
-        this.showFileList = true;
-        this.fileList.pop();
-        this.fileList.push({name: file.name, url: this.dataObj.host + '/' + this.dataObj.key.replace("${filename}",file.name) });
-        this.emitInput(this.fileList[0].url);
-      }
-    }
-  }
+      this.emitInput(this.fileList[0].url);
+    },
+  },
+};
 </script>
 <style>
-
 </style>
