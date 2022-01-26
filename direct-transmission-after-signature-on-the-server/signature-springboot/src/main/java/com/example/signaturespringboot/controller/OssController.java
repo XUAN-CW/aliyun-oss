49f60b06ca950c69dd6aa70339ebb49ba782aea5
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-01-26 22:32:19 +0800

    增大过期时间

--- direct-transmission-after-signature-on-the-server/signature-springboot/src/main/java/com/example/signaturespringboot/controller/OssController.java
+++ direct-transmission-after-signature-on-the-server/signature-springboot/src/main/java/com/example/signaturespringboot/controller/OssController.java
@@ -47,7 +47,7 @@ public class OssController {
         Map<String, String> respMap = new LinkedHashMap<String, String>();
 
         try {
-            long expireTime = 30;
+            long expireTime = 3000;
             long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
             Date expiration = new Date(expireEndTime);
             PolicyConditions policyConds = new PolicyConditions();
